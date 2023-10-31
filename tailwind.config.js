65624c58afa96e6f9a1a771b06da305537545779
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2023-10-31 16:11:46 +0100

    restoring original files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    fontFamily: {
-      Abril : ['Abril Fatface', 'serif'],
-      poppins : ['poppins'],
-      'a'
-    },
     extend: {}
   },
   darkMode: "class",
