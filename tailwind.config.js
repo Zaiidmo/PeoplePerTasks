ecce79e18725803e7a3e721ac98d9753696eaa9d
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2023-11-01 11:51:17 +0100

    i added the html and css of first section of dashboard (the cards)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '1/2': '50%',
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
+
   },
   darkMode: "class",
 };
