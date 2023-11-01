8b7df7aaceec22b9008507e9635d9733159866a7
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2023-11-01 14:12:21 +0100

    edditing the existing sections styles and adding the themes mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          50: "#fffbeb",
+          100: "#fef3c7",
+          200: "#fde68a",
+          300: "#fcd34d",
+          400: "#fbbf24",
+          500: "#f59e0b",
+          600: "#d97706",
+          700: "#b45309",
+          800: "#92400e",
+          900: "#78350f",
+          950: "#451a03",
+        },
+      },
+    }
   },
   darkMode: "class",
 };
