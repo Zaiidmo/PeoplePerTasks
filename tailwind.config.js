02d17d911d0f4490c2f8ef3c76fdac1e9810a369
Basma <basmaelandalosy5@gmail.com>
2023-11-03 14:49:44 +0100

    last edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 / @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/.js"],
+  darkMode: "class",
   theme: {
 
     extend: {
